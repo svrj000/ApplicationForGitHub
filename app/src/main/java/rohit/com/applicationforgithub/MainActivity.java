543b0eb84df8b9202a7975c7265460d94981d475
Rohit907512 <rohit9075@gmail.com>
2018-01-26 15:21:51 +0530

    Comment by rohit
    comment

--- app/src/main/java/rohit/com/applicationforgithub/MainActivity.java
+++ app/src/main/java/rohit/com/applicationforgithub/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//comment by rohit 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
