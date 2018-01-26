9d2cf3b8411f0bf52d6a2e6fee9be19d0bacc702
Rohit907512 <35831380+Rohit0098@users.noreply.github.com>
2018-01-26 14:23:13 +0530

    Initial commit

---
+++ app/src/main/java/rohit/com/applicationforgithub/MainActivity.java
@@ -0,0 +1,13 @@
+package rohit.com.applicationforgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
