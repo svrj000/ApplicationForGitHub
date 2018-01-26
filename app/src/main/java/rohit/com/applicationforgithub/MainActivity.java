e26241d404d57f2971bb50ba21eb2f92bae55e8c
Rohit907512 <rohit9075@gmail.com>
2018-01-26 15:35:12 +0530

    comment
    comment

--- app/src/main/java/rohit/com/applicationforgithub/MainActivity.java
+++ app/src/main/java/rohit/com/applicationforgithub/MainActivity.java
@@ -4,14 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-// Againg comment by Rohit
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test
     }
 }
