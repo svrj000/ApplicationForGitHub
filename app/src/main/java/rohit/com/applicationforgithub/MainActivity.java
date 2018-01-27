da12ea58c55cafd0c4ade74059205c3c313dc054
Silajit Sil <silajit42@gmail.com>
2018-01-27 22:31:02 +0530

    Testing Comment

--- app/src/main/java/rohit/com/applicationforgithub/MainActivity.java
+++ app/src/main/java/rohit/com/applicationforgithub/MainActivity.java
@@ -4,21 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-// Comment by Rohit by another github account
-
-    // Testing in New Branch
-
-//comment by rohit
-
-    // again comment by rohit
-    // again comment by silajit
 
 
 
 public class MainActivity extends AppCompatActivity {
-    // Test
-    //vishnu
-    // Silajit, Vishnu, Rohit, Jeevan
+
+
 
 
     @Override
